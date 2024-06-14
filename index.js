2a46a2432fdd61e6822fc047464e84cbd67f30a5
Jujuzix <maronez.juliana@gmail.com>
2024-06-14 16:15:33 -0400

    Inserção da rota para atualiazar a pagina e para excluir

--- index.js
+++ index.js
@@ -17,51 +17,69 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ urlencoded: true }));
 app.use(express.json());
 
-app.get("/usuario/novo", (req, res) => {
+app.get("/usuarios/formularioUsuario", (req, res) => {
     res.render(`formularioUsuario`);
+})
+
+app.post("/usuarios/formularioUsuario", async (req, res) => {
+    const dadosUsuario = {
+        nickname: req.body.nickname,
+        nome: req.body.nome,
+    };
+    const usuario = await Usuario.create(dadosUsuario);
+
+    res.send(`Usuário criado com o id ${usuario.id} <a href="/usuarios/formularioUsuario">Voltar</a> `)
 })
 
 app.get("/", (req, res) => {
     res.render(`home`);
 })
 
-app.get("/usuarios", (req, res) => {
-    res.render(`usuarios`);
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await Usuario.findAll({ raw: true });
+    res.render("usuarios", { usuarios })
 })
 
-// Rota para ser exibido o formulário para ser inserido os dados
-app.get("/jogos/novo", (req, res) => {
-    res.sendFile(`${__dirname}/views/formularioJogos.html`);
+app.get("/usuarios/:id/atualizar", async (req, res) => {
+    const id = req.params.id
+    const usuario = await Usuario.findByPk(id, { raw: true })
+    res.render("formularioUsuario", { usuario });
 })
 
-//Rota para criar um novo dado de jogo    
-app.post("/jogos/novo", async (req, res) => {
-    const dadosJogos = {
-        titulo: req.body.titulo,
-        descricao: req.body.descricao,
-        preco: req.body.preco
-    };
-
-    const jogos = await Jogos.create(dadosJogos);
-    res.send("Jogo inserido sob o id" + jogos.id);
-})
 
 //Rota para criar um novo dado de um usuario    
-app.post("/usuario/novo", async (req, res) => {
+app.post("/usuarios/:id/atualizar", async (req, res) => {
+    const id = req.params.id;
+
     const dadosUsuario = {
         nickname: req.body.nickname,
-        nome: req.body.name
+        nome: req.body.nome
+    }
+    const registroAfetados = await Usuario.update(dadosUsuario, { where: { id: id } })
+    if (registroAfetados > 0) {
+        res.redirect("/usuarios");
+    } else {
+        res.send("Erro ao atualizar usuário!")
+    }
+});
 
-    };
+app.post("/usuarios/excluir", async (req, res) => {
+    const id = req.body.id;
 
-    const usuario = await Jogos.create(dadosUsuario);
-    res.send("Usuario inserido sob o id" + usuario.id);
+    const registroAfetados = await Usuario.destroy({
+        where: { id: id },
+    })
+    if (registroAfetados > 0) {
+        res.redirect("/usuarios");
+    } else {
+        res.send("Erro ao excluir usuário!")
+    }
 })
-
 
 //Comando para iniciar o servidor
 app.listen(8000, () => {
     console.log("Server rodando!");
+    console.log("http://localhost:8000/")
 })
 
 // Sincroniza o modelo com o seu banco de dados
@@ -86,3 +104,22 @@ conn
     .catch((err) => {
         console.log('Ocorreu um erro' + err);
     })
+
+
+
+/*app.get("/jogos/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formularioJogos.html`);
+})
+
+//Rota para criar um novo dado de jogo    
+app.post("/jogos/novo", async (req, res) => {
+    const dadosJogos = {
+        titulo: req.body.titulo,
+        descricao: req.body.descricao,
+        preco: req.body.preco
+    };
+
+    const jogos = await Jogos.create(dadosJogos);
+    res.send("Jogo inserido sob o id" + jogos.id);
+})
+*/
